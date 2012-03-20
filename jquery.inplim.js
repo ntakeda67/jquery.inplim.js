939bad4e3410eda0d3fe79d49c0af15cc887b906
nt67 <natsuki@gwnt67.nt67group>
2012-03-20 20:02:21 +0900

    implement restriction for japanese.

--- jquery.inplim.js
+++ jquery.inplim.js
@@ -12,24 +12,46 @@
 	* @return {Boolean} true: restriction enabled, false:disabled
 	*/
        inplim : function(options){
+	 var defaults = {
+	   jp: false,
+	   regexStr: '.*'
+	 };
 	 if(!options){
 	   return false;
 	 }
 
-	 var regexStr = options.regexStr || '.*';
-	 var regex = new RegExp(regexStr);
+	 var that = this;
+	 var options = $.extend(defaults, options);
+	 var regex = new RegExp(options.regexStr);
 	 $(this).keypress(restrict);
-	 $(this).keyup(function(event){
-			 //13だけひろう
-		       });
+	 if(options.jp){
+	   $(this).keyup(jpRestrict);
+	 }
 
 	 return true;
+
+	 /**
+	  * 日本語入力確定をトリガーとして、
+	  * テキストボックス全体の入力を検証する。
+	  */
+	 function jpRestrict(event){
+	   var code = getInputCode(event);
+	   if(code === 13){
+	     var corrected = '';
+	     $.each($(that).val(), function(){
+		      corrected += regex.test(this) ? this : '';
+		      console.log(this);
+		    });
+	     $(that).val(corrected);
+	   }
+	 }
+
 	 /**
 	  * stop to input not allowed character.
 	  * @param event {Object} event object.
 	  */
 	 function restrict(event){
-	   var code = event.which ? event.which : event.keyCode ? event.keyCode : event.charCode ? event.charCode : 0 ;
+	   var code = getInputCode(event);
 	   var ch = String.fromCharCode(code);
 	   if( (!code) || (code < 32) || (!regex.test(ch)) ) {
 	     event.preventDefault();
@@ -37,6 +59,14 @@
 	   }
 	   return true;
 	 }
+
+	 /**
+	  * get input key code;
+	  * @param event {Object} event object.
+	  */
+	 function getInputCode(event){
+	   return event.which ? event.which : event.keyCode ? event.keyCode : event.charCode ? event.charCode : 0 ;
+	   }
        }
      }
    );
